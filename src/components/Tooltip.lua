65f057d2b0520ea2fef22c9a4c5e6f26182cab1c
Tiffany Bennett <tiffany@stormbit.net>
2018-05-08 12:38:51 -0700

    Put tooltips at a fixed position since there's no reliable mouse events

--- src/components/Tooltip.lua
+++ src/components/Tooltip.lua
@@ -3,54 +3,24 @@ local Modules = script.Parent.Parent
 local Roact = require(Modules.Roact)
 local RoactRodux = require(Modules.RoactRodux)
 
-local Tooltip = Roact.Component:extend("Tooltip")
-
-function Tooltip:render()
-	local props = self.props
+local function Tooltip(props)
 	local tooltip = props.tooltip
 
-	if tooltip then
-		local size = 20
-		local font = Enum.Font.SourceSans
-		local TextService = game:GetService("TextService")
-		local bounds = TextService:GetTextSize(tooltip, size, font, Vector2.new(200, 400))
-		return Roact.createElement("TextLabel", {
-			Text = tooltip,
-			TextColor3 = Color3.fromRGB(0, 0, 0),
-			TextSize = size,
-			Font = font,
-			Size = UDim2.new(0, bounds.x + 16, 0, bounds.y + 16),
-			Position = UDim2.new(0, self.state.x + 16, 0, self.state.y),
-			BackgroundColor3 = Color3.fromRGB(255, 255, 240),
-		})
-	else
-		return nil
-	end
-end
-
-function Tooltip:init()
-	self.state = {
-		x = 0,
-		y = 0
-	}
-end
-
-function Tooltip:didMount()
-	local RunService = game:GetService("RunService")
-	local UserInputService = game:GetService("UserInputService")
-	local GuiService = game:GetService("GuiService")
-	self.conn = RunService.Heartbeat:Connect(function()
-		local inset = GuiService:GetGuiInset()
-		local mouse = UserInputService:GetMouseLocation() - inset
-		self:setState({
-			x = mouse.X,
-			y = mouse.Y,
-		})
-	end)
-end
-
-function Tooltip:willUnmount()
-	self.conn:Disconnect()
+	local size = 20
+	local font = Enum.Font.SourceSans
+	local TextService = game:GetService("TextService")
+	local bounds = TextService:GetTextSize(tooltip or "", size, font, Vector2.new(200, 400))
+	return Roact.createElement("TextLabel", {
+		Text = tooltip or "",
+		TextColor3 = Color3.fromRGB(0, 0, 0),
+		Visible = tooltip ~= nil,
+		TextSize = size,
+		Font = font,
+		Size = UDim2.new(0, bounds.x + 16, 0, bounds.y + 16),
+		Position = UDim2.new(1, -16, 1, -16),
+		AnchorPoint = Vector2.new(1, 1),
+		BackgroundColor3 = Color3.fromRGB(255, 255, 240),
+	})
 end
 
 Tooltip = RoactRodux.connect(function(store)
